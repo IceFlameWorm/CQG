526ef811d289e668faaf719a560c3899897883e8
aaron <aaronnli@deloitte.com.cn>
2019-06-06 17:56:28 +0800

    保存修改

--- cqg/ask.py
+++ cqg/ask.py
@@ -1,12 +1,12 @@
-from .analyze import chinese_cut_sent as cut_sent
-from stanfordcorenlp import StanfordCoreNLP
+from .analyze import chinese_cut_sent
+from .transform import SimpleTransformer
 
 
 class BaseAsker(object):
     def __init__(self):
         pass
 
-    def __call__(self, doc):
+    def __call__(self, para):
         raise NotImplementedError
 
 
@@ -14,29 +14,27 @@ class SimpleAsker(BaseAsker):
     def __init__(self,
                  stanford_model
                 ):
-        self.nlp = StanfordCoreNLP(stanford_model, lang = 'zh')
+        self.tranformer = SimpleTransformer(stanford_model)
 
     def close(self):
-        self.nlp.close()
+        self.tranformer.close()
+
 
     def __call__(self, para):
         """
         1. 分句 -> 句子列表
-        2. 对所有地句子进行句法解析 -> 解析树列表
-        3. transform产生可用于生成问题地候选句子
+        2. transform产生可用于生成问题地候选句子
             (1) 筛选不符合要求的句子
             (2) 句子简化
-        4. transducer用候选句子产生问句
-        5. ranker对产生的问题进行排序
+        3. transducer用候选句子产生问句
+        4. ranker对产生的问题进行排序
         """
 
         ## 分句
         source_sens = self.get_sentences(para)
 
-        ## 句法解析
-        trees = self.get_trees(source_sens)
-
         ## 产生候选句子
+        derived_sens = self.tranformer(source_sens)
 
         ## 产生问句
 
@@ -47,23 +45,18 @@ class SimpleAsker(BaseAsker):
     def get_sentences(self, para):
         """
         Args:
-            doc: str
+            para: str
         Returns:
             list[str]: list of sentences
         """
-        sentences = cut_sent(para)
+        sentences = chinese_cut_sent(para)
         return sentences
 
-    def get_trees(self, source_sens):
-        trees = []
-        for sen in source_sens:
-            tree = self.nlp.parse(sen)
-            trees.append(tree)
-        return trees
 
 
 if __name__ == "__main__":
-    doc = "中国日前发表《关于中美经贸磋商的中方立场》白皮书，以权威而翔实的数据与事实，完整准确还原了中美经贸磋商的过程。国际社会由此进一步认清美国出尔反尔、违背共识、不讲诚信的真相，但美方对此坐不住了。6月3日，美国中国日前发表《关于中美经贸磋商的中方立场》白皮书，以权威而翔实的数据与事实，完整准确还原了中美经贸磋商的过程。国际社会由此进一步认清美国出尔反尔、违背共识、不讲诚信的真相，但美方对此坐不住了。6月3日，美国贸易代表办公室和财政部就中方立场白皮书发表声明，污称中国“玩推卸责任的游戏”“歪曲两国贸易谈判的性质和历史”。声明的字里行间，又进一步暴露了美方一贯歪曲事实、倒打一耙的真实面目。贸易代表办公室和财政部就中方立场白皮书发表声明，污称中国“玩推卸责任的游戏”“歪曲两国贸易谈判的性质和历史”。声明的字里行间，又进一步暴露了美方一贯歪曲事实、倒打一耙的真实面目。"
-    stanford_model = '~/model_files/stanfordcorenlp/stanford-corenlp-full-2018-02-27'
+    para = "中国日前发表《关于中美经贸磋商的中方立场》白皮书，以权威而翔实的数据与事实，完整准确还原了中美经贸磋商的过程。国际社会由此进一步认清美国出尔反尔、违背共识、不讲诚信的真相，但美方对此坐不住了。6月3日，美国中国日前发表《关于中美经贸磋商的中方立场》白皮书，以权威而翔实的数据与事实，完整准确还原了中美经贸磋商的过程。国际社会由此进一步认清美国出尔反尔、违背共识、不讲诚信的真相，但美方对此坐不住了。6月3日，美国贸易代表办公室和财政部就中方立场白皮书发表声明，污称中国“玩推卸责任的游戏”“歪曲两国贸易谈判的性质和历史”。声明的字里行间，又进一步暴露了美方一贯歪曲事实、倒打一耙的真实面目。贸易代表办公室和财政部就中方立场白皮书发表声明，污称中国“玩推卸责任的游戏”“歪曲两国贸易谈判的性质和历史”。声明的字里行间，又进一步暴露了美方一贯歪曲事实、倒打一耙的真实面目。"
+    import os
+    stanford_model = os.path.join(os.environ["HOME"], 'model_files/stanfordcorenlp/stanford-corenlp-full-2018-02-27')
     asker = SimpleAsker(stanford_model)
-    print(asker.get_sentences(doc))
+    asker(para)
--- cqg/transform.py
+++ cqg/transform.py
@@ -1,3 +1,5 @@
+from stanfordcorenlp import StanfordCoreNLP
+
 class TransformResult(object):
     def __init__(self, source, derived):
         self.source = source
@@ -11,14 +13,27 @@ class BaseTransformer(object):
     def __init__(self):
         pass
 
-    def __call__(self, source):
+    def __call__(self, source_sens):
         raise NotImplementedError
 
 
 class SimpleTransformer(BaseTransformer):
-    def __init__(self):
-        pass
+    def __init__(self, stanford_model,
+                 max_len = 50,
+                ):
+        self.nlp = StanfordCoreNLP(stanford_model, lang = "zh")
+        self.max_len = max_len
 
-    def __call__(self, source):
-        res = TransformResult(source, [])
+    def __call__(self, source_sens):
+        ## 句子筛选
+        kepted_sens = self.filter_sens(source_sens)
+        # 句子简化
+        res = None
         return res
+
+    def close(self):
+        self.nlp.close()
+
+    def filter_sens(self, source_sens):
+        kepted_sens = [sen for sen in source_sens if len(sen) <= self.max_len]
+        return kepted_sens
